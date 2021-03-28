b3ea7cc9d306b1a3fea5e9a8d62b1f2ed3ec4fd6
malikateeq <malikateeq@vaultspay.ae>
2021-03-28 09:35:08 +0400

    React Fragments

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -9,15 +9,16 @@ const Modal = (props) => {
                 <div className="modal-dialog modal-dialog-centered">
                     <div className="modal-content">
                     <div className="modal-header">
-                        <h5 className="modal-title" id="exampleModalLabel">Delete Stream</h5>
+                        <h5 className="modal-title" id="exampleModalLabel">
+                            { props.title }
+                        </h5>
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div className="modal-body">
-                        Are you sure you want to delete this stream?
+                        { props.content }
                     </div>
                     <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                        <button type="button" className="btn btn-primary">Delete</button>
+                        { props.actions }
                     </div>
                     </div>
                 </div>
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -2,6 +2,12 @@ import React from 'react';
 import Modal from "../Modal";
 
 export default function streamDelete() {
+    const modalAction = (
+        <React.Fragment>
+            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+            <button type="button" className="btn btn-primary">Delete</button>
+        </React.Fragment>
+    );
     return (
         <div>
             Delete Modal
@@ -10,7 +16,11 @@ export default function streamDelete() {
                 Launch demo modal
             </button>
 
-            <Modal />
+            <Modal
+                title="Delete Stream"
+                content="Are you sure you want to delete this stream?"
+                actions={modalAction}
+            />
         </div>
     )
 }
